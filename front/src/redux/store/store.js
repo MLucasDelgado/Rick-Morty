7ba4df7f47861ca4bd4f6a02e6536694d00ada6b
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-03-17 18:08:09 -0300

    correcion de la store para la extension del navegador

--- front/src/redux/store/store.js
+++ front/src/redux/store/store.js
@@ -1,14 +1,13 @@
-import { createStore, applyMiddleware, compose} from "redux";
-import thunkMiddleware from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk"; // Usa "thunk" en lugar de "thunkMiddleware"
 import reducer from "../reducer/reducer";
 
-// Para que la extension del navegador funcione
-
+// Habilita Redux DevTools si está disponible
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     reducer,
-    composeEnhancer(applyMiddleware(thunkMiddleware))
+    composeEnhancer(applyMiddleware(thunk)) // Cambiado a thunk
 );
 
 export default store;
