21780dfc92bb196a675840d83dae459845c54305
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-03-17 18:07:15 -0300

    creacion del reducer con lista de personajes favoritos

--- front/src/redux/reducer/reducer.js
+++ front/src/redux/reducer/reducer.js
@@ -1,9 +1,28 @@
-const initialState = {
+import { ADD_FAV, REMOVE_FAV } from "../actions-type/actions-type"
 
+const initialState = {
+    myFavorites: []
 }
 
 const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_FAV:
+            return {
+                ...state,
+                myFavorites: [...state.myFavorites, action.payload]
+            }
 
+        case REMOVE_FAV:
+            return {
+                ...state,
+                myFavorites: state.myFavorites.filter((character) => character.id != action.payload)
+            }
+        
+        default:
+            return {
+                ...state
+            }
+    }
 }
 
 export default reducer
