2b47c26d0d09de7d0da31518dd1b86a14329a94d
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-03-18 15:33:07 -0300

    agregado de los casos FILTER y ORDER

--- front/src/redux/reducer/reducer.js
+++ front/src/redux/reducer/reducer.js
@@ -1,23 +1,55 @@
-import { ADD_FAV, REMOVE_FAV } from "../actions-type/actions-type"
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "../actions-type/actions-type"
 
 const initialState = {
+    allCharacters: [],
     myFavorites: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_FAV:
+            const updatedFavorites = [...state.myFavorites, action.payload];
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
-            }
+                myFavorites: updatedFavorites, // esta nos mostrara los cambios que hicimos
+                allCharacters: updatedFavorites // guardamos esta copia para hacer los cambios
+            };
 
         case REMOVE_FAV:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter((character) => character.id != action.payload)
             }
-        
+
+        case FILTER:
+            let charactersFiltered;
+
+            if (action.payload === 'All') {
+                charactersFiltered = state.allCharacters; // Mostrar todos los personajes
+            } else {
+                charactersFiltered = state.allCharacters.filter(
+                    (character) => character.gender === action.payload
+                );
+            }
+
+            return {
+                ...state,
+                myFavorites: charactersFiltered
+            }
+
+        case ORDER:
+            let orderCharacters = [...state.allCharacters]
+            if (action.payload === 'Ascendente') {
+                orderCharacters.sort((charater1, character2) => charater1.id - character2.id) // accedo a la propiedad id del character
+            }
+            if (action.payload === 'Descendente') {
+                orderCharacters.sort((charater1, charater2) => charater2.id - charater1.id)
+            }
+            return {
+                ...state,
+                myFavorites: orderCharacters
+            }
+
         default:
             return {
                 ...state
