cb2e78eb0784800bbdf32890aec9ef0389a9792e
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-03-18 15:31:29 -0300

    filtrado por orden o genero

--- front/src/components/Favorites/Favorites.jsx
+++ front/src/components/Favorites/Favorites.jsx
@@ -1,4 +1,6 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { useState } from "react"
+import { filterCards, orderCards } from "../../redux/actions/actions"
 import Card from "../Card/Card"
 
 const Favorites = ({onClose}) => {
@@ -17,8 +19,35 @@ const Favorites = ({onClose}) => {
             />
         )
     })
+
+    const dispatch = useDispatch()
+
+    // creamos este estado para que cuando utilicemos el metodo sort en el reducer el componente se re-renderice y nos muestre el filtrado
+    const [aux, setAux] = useState(false)
+
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value))
+        setAux(!aux)
+    }
+
+    const handleFilter = (event) => {
+        dispatch(filterCards(event.target.value))
+    }
+
     return (
         <div>
+            <select name="orden" onChange={handleOrder}>
+                <option value="Ascendente">Ascendente</option>
+                <option value="Descendente">Descendente</option>
+            </select>
+
+            <select name="generos" onChange={handleFilter}>
+                <option value="All">All</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">Unknown</option>
+            </select>
             {likedCharacter}
         </div>
     )
