2bc5720a927344988bac2f0ca71d3caabc7e1f53
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-04-07 12:43:43 -0300

    modificamos la funcion con express

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,26 +1,29 @@
 const axios = require("axios")
 
-const getCharById = (response, id) => {
-    axios(`https://rym2.up.railway.app/api/character/${id}?key={tuApiKey}`)
+const getCharById = (request, response) => {
+    const { id } = request.params
+    const url = `https://rym2.up.railway.app/api/character/${id}?key={tuApiKey}`
+    
+    axios.get(url)
     .then(({ data }) => {
-        const character = {
-            id: id,
-            name: data.name,
-            gender: data.gender,
-            species: data.species,
-            origin: data.origin.name,
-            image: data.image,
-            status: data.status
+        if (data.name) {
+            const character = {
+                id: data.id,
+                name: data.name,
+                gender: data.gender,
+                species: data.species,
+                origin: data.origin.name,
+                image: data.image,
+                status: data.status
+            }
+            response.json(character)
+        } else {
+            response.status(404).send("Not found")
         }
-        
-        response.writeHead(200, {"Content-type": "application/json"})
-        response.end(JSON.stringify(character))
     })
-
     .catch((error) => {
-        response.writeHead(500, {"Content-type": "text/plain"})
-        response.end(error.message)
-    })
+        response.status(500).send(error.message);
+    });
 }
 
 module.exports = getCharById
