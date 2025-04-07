8706c5b4e53a697f9a9b9ff3bc9373d3d776152b
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-04-07 12:44:07 -0300

    Creamos el servidor con express

--- back/src/index.js
+++ back/src/index.js
@@ -1,16 +1,28 @@
-const http = require('http');
+const express = require("express");
+const router = require("./routes/index");
+const server = express();
 const PORT = 3001;
-const getCharById = require('./controllers/getCharById')
 
-http.createServer((request, response) => {
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    if(request.url.includes("/rickandmorty/character")) {
-        // Obtener el ID de la URL
-        let arrayUrl = request.url.split('/'); // Divide la URL en partes
-        let id = arrayUrl[arrayUrl.length - 1]; // Obtiene el último elemento
-        let numId = Number(id); // Convierte a número
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
 
-        getCharById(response, numId)
-    }
-    
-}).listen(PORT, "localhost")
+// Body parser
+server.use(express.json());
+
+// Tus rutas
+server.use("/rickandmorty", router);
+
+server.listen(PORT, () => {
+    console.log('Server raised in port: ' + PORT);
+});
